6ab4e5b6f19f47bba9e85be56ed00baf09211da7
Erik <75934309+Vorkys@users.noreply.github.com>
2024-02-23 13:32:38 +0100

    first commit

---
+++ tahova_RPG_hra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tahova_RPG_hra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
