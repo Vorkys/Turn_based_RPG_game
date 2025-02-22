ddbd7384c96cbd95980fb1d81c4590551e74971d
Erik <75934309+Vorkys@users.noreply.github.com>
2025-02-22 10:58:30 +0100

    implemented Location (https://github.com/Vorkys/tahova-RPG-hra/issues/2)

--- tahova_RPG_hra/Source/Locations/Location.cs
+++ tahova_RPG_hra/Source/Locations/Location.cs
@@ -8,9 +8,39 @@ namespace tahova_RPG_hra.Source.Locations
 {
     internal class Location
     {
+        private Node[,] location;
         private int entryX;
         private int entryY;
         private int playerX;
         private int playerY;
+
+        public Location(Node[,] location, int entryX, int entryY, int playerX, int playerY)
+        {
+            this.location = location;
+            this.entryX = entryX;
+            this.entryY = entryY;
+            this.playerX = playerX;
+            this.playerY = playerY;
+        }
+
+        public Location(Node[,] location, int entryX, int entryY)
+        {
+            this.location = location;
+            this.entryX = entryX;
+            this.entryY = entryY;
+            this.playerX = -1;
+            this.playerY = -1;
+        }
+
+        public void writeMap()
+        {
+            foreach (Node mapRow in location)
+            {
+                foreach (Nodes mapNode in mapRow)
+                    mapNode.write();
+
+                Console.WriteLine();
+            }
+        }
     }
 }
